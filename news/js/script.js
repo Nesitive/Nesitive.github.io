b1d311e9debb07016c13c20b09b883021ac19d6d
Nesitive <wbeau12@yahoo.com>
2025-03-26 14:39:51 -0500

    Add fullscreen button

--- news/js/script.js
+++ news/js/script.js
@@ -7,11 +7,15 @@ PlayButtonIcon = document.getElementById("play-button-icon");
 VideoBarFill = document.getElementById("video-bar-fill");
 VideoBarTextLeft = document.getElementById("video-bar-text-left");
 VideoBarTextRight = document.getElementById("video-bar-text-right");
+VideoWrapper = document.getElementById("video-wrapper");
 VolumeButton = document.getElementById("volume-button");
 VolumeSlider = document.getElementById("volume-slider");
 VolumeSliderInput = document.getElementById("volume-slider-input");
 
 // Variables
+BottomBarAutoHideDuration = 3000;
+BottomBarHideTime = Number.MAX_SAFE_INTEGER;
+ForceBarVisibility = false;
 SegmentNumber = 0;
 VideoBarDragging = false;
 VideoDuration = parseInt(VideoWrapper.dataset.duration);
@@ -35,10 +39,13 @@ setInterval(() => {
             width: (100 * (((120 * SegmentNumber) + CurrentVideo.currentTime) / VideoDuration)) + "%"
         }, {duration: 100, fill: "forwards"});
         VideoBarTextLeft.innerHTML = ToMMSS((120 * SegmentNumber) + CurrentVideo.currentTime);
-        VideoBarTextRight.innerHTML = ToMMSS(CurrentVideo.currentTime - VideoDuration);
+        VideoBarTextRight.innerHTML = ToMMSS((120 * SegmentNumber) + CurrentVideo.currentTime - VideoDuration);
         if ((CurrentVideo.duration - CurrentVideo.currentTime) < 0.2) {
             PlayNextSegment();
         }
+    }
+    if (BottomBarHideTime < new Date().getTime() && !ForceBarVisibility) {
+        BottomBar.classList.remove("active");
     }
 }, 100);
 VideoBarFill.parentNode.addEventListener("mousedown", (event) => {
@@ -70,6 +77,7 @@ VideoBarFill.parentNode.addEventListener("touchend", (event) => {
 VolumeSliderInput.value = CurrentVideo.volume;
 VolumeSliderInput.oninput = () => {
     CurrentVideo.volume = VolumeSliderInput.value;
+    BottomBarHideTime = new Date().getTime() + BottomBarAutoHideDuration;
 }
 VolumeButton.onclick = () => {
     if (VolumeSlider.classList.contains("active")) {
@@ -78,12 +86,20 @@ VolumeButton.onclick = () => {
     else {
         VolumeSlider.classList.add("active");
     }
+    BottomBarHideTime = new Date().getTime() + BottomBarAutoHideDuration;
 }
 document.onclick = (event) => {
+    BottomBarHideTime = new Date().getTime() + BottomBarAutoHideDuration;
     ToggleBottomBar(event);
 }
 BottomBar.onclick = (event) => {
     event.stopPropagation();
+}
+BottomBar.onmouseover = () => {
+    ForceBarVisibility = true;
+}
+BottomBar.onmouseleave = () => {
+    ForceBarVisibility = false;
 }
 
 
@@ -97,9 +113,15 @@ function LoadVideo() {
 }
 
 function PlayNextSegment() {
+    console.log(SegmentNumber, parseInt(VideoWrapper.dataset.segments));
+    if (SegmentNumber + 1 >= VideoWrapper.dataset.segments) {
+        return;
+    }
     LoadVideo();
+    if (SegmentNumber + 2 < parseInt(VideoWrapper.dataset.segments)) {
+        PreloadVideo(CurrentVideo.src.substring(0, CurrentVideo.src.indexOf("-") + 1) + (parseInt(CurrentVideo.src.substring(CurrentVideo.src.indexOf("-") + 1)) + 1) + ".webm");
+    }
     SegmentNumber++;
-    PreloadVideo(CurrentVideo.src.substring(0, CurrentVideo.src.indexOf("-") + 1) + (parseInt(CurrentVideo.src.substring(CurrentVideo.src.indexOf("-") + 1)) + 1) + ".webm");
     CurrentVideo.play();
 }
 
@@ -136,6 +158,7 @@ function ToMMSS(n) {
 }
 
 function VideoBarSeek(clientX) {
+    BottomBarHideTime = new Date().getTime() + BottomBarAutoHideDuration;
     rect = VideoBarFill.parentNode.getBoundingClientRect();
     left = rect.left - (parseFloat(getComputedStyle(document.documentElement).fontSize) * 0.125);
     width = rect.width - (parseFloat(getComputedStyle(document.documentElement).fontSize) * 0.25);
@@ -152,6 +175,18 @@ function VideoBarSeek(clientX) {
     VideoSeek(duration);
 }
 
+function ToggleVideoFullscreen() {
+    if (FullscreenButton.classList.contains("active")) {
+        FullscreenButton.classList.remove("active");
+        document.body.requestFullscreen();
+        document.exitFullscreen();
+    }
+    else {
+        FullscreenButton.classList.add("active");
+        document.body.requestFullscreen();
+    }
+}
+
 function VideoPlayPause() {
     if (CurrentVideo.paused) {
         CurrentVideo.play();
@@ -161,6 +196,7 @@ function VideoPlayPause() {
         CurrentVideo.pause();
         PlayButtonIcon.classList.add("paused");
     }
+    BottomBarHideTime = new Date().getTime() + BottomBarAutoHideDuration;
 }
 
 function VideoSeek(duration) {
@@ -168,11 +204,13 @@ function VideoSeek(duration) {
     VideoSource = "video/" + VideoID + "-" + SegmentNumber + ".webm"
     if (!CurrentVideo.src.endsWith(VideoSource)) {
         CurrentVideo.src = VideoSource;
-        PreloadVideo(CurrentVideo.src.substring(0, CurrentVideo.src.indexOf("-") + 1) + (parseInt(CurrentVideo.src.substring(CurrentVideo.src.indexOf("-") + 1)) + 1) + ".webm");
+        if (SegmentNumber + 2 < parseInt(VideoWrapper.dataset.segments)) {
+            PreloadVideo(CurrentVideo.src.substring(0, CurrentVideo.src.indexOf("-") + 1) + (parseInt(CurrentVideo.src.substring(CurrentVideo.src.indexOf("-") + 1)) + 1) + ".webm");
+        }
     }
     CurrentVideo.currentTime = duration - (120 * SegmentNumber);
     VideoBarTextLeft.innerHTML = ToMMSS((120 * SegmentNumber) + CurrentVideo.currentTime);
-    VideoTimeRemaining = ToMMSS(CurrentVideo.currentTime - VideoDuration);
+    VideoTimeRemaining = ToMMSS((120 * SegmentNumber) + CurrentVideo.currentTime - VideoDuration);
     if (!VideoTimeRemaining.startsWith("-")) {
         VideoTimeRemaining = "-" + VideoTimeRemaining;
     }
