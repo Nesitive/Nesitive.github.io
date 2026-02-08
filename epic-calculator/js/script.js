d57f40281d194e856ccc36a12bd27a378f410eef
Nesitive <wbeau12@yahoo.com>
2026-02-08 17:22:20 -0600

    Added calculation and started on history tape

--- epic-calculator/js/script.js
+++ epic-calculator/js/script.js
@@ -1,4 +1,23 @@
 CurrentExpression = document.getElementById("current-expression");
+History = document.getElementById("history");
+
+function Calculate() {
+  HistoryItem = document.createElement("div");
+  HistoryItem.classList.add("history-item");
+
+  HistoryInput = document.createElement("div");
+  HistoryInput.classList.add("history-input");
+  HistoryInput.innerHTML = CurrentExpression.value;
+
+  HistoryOutput = document.createElement("div");
+  HistoryOutput.classList.add("history-output");
+  HistoryOutput.innerHTML = "=" + eval(CurrentExpression.value);
+
+  InputClear();
+  HistoryItem.appendChild(HistoryInput);
+  HistoryItem.appendChild(HistoryOutput);
+  History.appendChild(HistoryItem);
+}
 
 function InputAppend(text) {
   CurrentExpression.value += text;
